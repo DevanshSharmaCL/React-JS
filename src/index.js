c46f83cc7ebc28de65815c934223ec6d107f8835
DevanshSharmaCL <devanshsharma70cl@gmail.com>
2025-02-24 12:24:09 +0530

    Day-7 By using useRef,created 2 examples and one video player example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Counter from "./All Topics/useReducer/Ecample-2";
+import Video_Player_Controller from "./All Topics/UseRef/Video_Player_Controller";
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Corrected
 root.render(
     <React.StrictMode>
-        <Counter />
+        <Video_Player_Controller />
     </React.StrictMode>
 );
  
