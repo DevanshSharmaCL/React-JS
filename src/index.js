bbc28b08bbab5b82a33cd76a53cec6cd7dd239b5
DevanshSharmaCL <devanshsharma70cl@gmail.com>
2025-02-25 17:49:37 +0530

    Day8 higher order function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Counter from "./All Topics/Custom Hooks/UseCounter";
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Corrected
 root.render(
     <React.StrictMode>
-        <Counter />
+       
     </React.StrictMode>
 );
  
